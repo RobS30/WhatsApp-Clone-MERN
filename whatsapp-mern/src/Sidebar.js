da399d45ee8fbaf6bd24195428a80a674713a843
RobS <51037713+RobS30@users.noreply.github.com>
2020-09-12 22:34:44 -0500

    added avatar to sidebar header component

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Sidebar.css";
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import { Avatar, IconButton } from "@material-ui/core";
+// avatar is a component that can take a source from an image that you're going to use
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 
@@ -9,6 +10,7 @@ function Sidebar() {
     return (
         <div className='sidebar'>
             <div className='sidebar_header'>
+                <Avatar src ="https://avatars0.githubusercontent.com/u/51037713?s=460&v=4"></Avatar>
                 <div className='sidebar_headerRight'>  
                     <IconButton>
                     <DonutLargeIcon />
