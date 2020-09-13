05799894e555007e2fac833a9aa35f864f2497b4
RobS <51037713+RobS30@users.noreply.github.com>
2020-09-12 22:13:58 -0500

    added additional icons to sidebar component

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -2,7 +2,8 @@ import React from 'react'
 import "./Sidebar.css";
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import { Avatar, IconButton } from "@material-ui/core";
-// import ChatIcon from '@material-ui/icons/Chat';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function Sidebar() {
     return (
@@ -11,7 +12,14 @@ function Sidebar() {
                 <div className='sidebar_headerRight'>  
                     <IconButton>
                     <DonutLargeIcon />
+                    </IconButton>  
+                    <IconButton>
+                    <ChatIcon />
+                    </IconButton>  
+                    <IconButton>
+                    <MoreVertIcon />
                     </IconButton>       
+
                 </div>
 
             </div>
